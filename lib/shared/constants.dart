305b7f73c48126eacc188b31af30bcfea06b60e4
BAD4SSDUCK <daksha.krishnan@gmail.com>
2023-01-11 14:40:31 +0200

    Initial commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:13974674947:android:d79638e6649b8d2617f836";
+  static String apiKey = "AIzaSyA_XlyHsZ7ZeGUk3QzMl83hDC8IxGnsEi0";
+  static String messagingSenderId = "634480644405";
+  static String projectId = "flutterchatapp-251dc";
+  final primaryColor = const Color(0xFF50C878);
+}
